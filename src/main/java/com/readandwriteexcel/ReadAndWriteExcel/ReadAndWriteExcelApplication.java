badba8e3cd44ea7141ea2d0448af552780f0c488
Nischith <Nischith@DESKTOP-RSRN1S3>
2024-03-16 09:41:22 +0530

    initial commit

---
+++ src/main/java/com/readandwriteexcel/ReadAndWriteExcel/ReadAndWriteExcelApplication.java
@@ -0,0 +1,13 @@
+package com.readandwriteexcel.ReadAndWriteExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadAndWriteExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadAndWriteExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readandwriteexcel/ReadAndWriteExcel/ReadAndWriteExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readandwriteexcel.ReadAndWriteExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadAndWriteExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
